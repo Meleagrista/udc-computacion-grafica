ea29c8bd65b4db36d19d5937b22882c5609846a9
Meleagrista <martin.dorio@udc.es>
2023-10-10 17:04:11 +0200

    Execuatbale submitted.

--- e2/p1.c
+++ e2/p1.c
@@ -4,14 +4,20 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+/* CONTROLES:
+ * 1. Las teclas de la práctica anterior mueven las piezas en los tres ejes.
+ * 2. La tecla '1' selecciona la siguiente pieza para más información leer comentarios de la función display().
+ * 3. Abrir el menú funciona como se describe en la práctica
+ * 4. Hacer click derecho y arrastrar mueve la pieza seleccioanda al soltar el click.
+ * 5. Pulsar CTRL selecciona la pierna izquierda, pulsar ALT selecciona la pierna derecha y si no se pulsa ninguna el cuerpo enterio es seleccionado.
+ */
+
+GLboolean wired = GL_FALSE;
+GLboolean spin = GL_FALSE;
+GLfloat spinSpeed = 2.0f;
+
 GLint ancho = 400;
 GLint alto = 400;
-
-/* Idealmente... 
- * 1. Tendría que guardar cada pieza o mas bien grupo de piezas: esfera + cubo como instancias de una clase.
- * 2. Añadir a dicho 'prefab' las variables que la afectan, en este caso los angulos.
- * 3. Crear una agrupación mayor donde meter todos los componentes y donde poder seleccionarlos.
- */
 
 int piece_index = 0;
 int pieces = 0;
@@ -40,7 +46,7 @@ void reshape(int width, int height)
     alto = height;
 }
 
-void drawShiftedSphere(GLfloat x, GLfloat y, GLfloat z) {
+void drawJoint(GLfloat x, GLfloat y, GLfloat z) {
     glMatrixMode(GL_MODELVIEW);
 
     glPushMatrix();
@@ -50,13 +56,17 @@ void drawShiftedSphere(GLfloat x, GLfloat y, GLfloat z) {
     glRotatef(anglesZ[pieces + arm_index], 0.0f, 0.0f, 1.0f);
     
     glColor3f(1.0f, 0.0f, 0.0f); // Color rojo
-    glutSolidSphere(0.5, 30, 30);
+    
+    if(wired)
+        glutWireSphere(0.5, 10, 10);
+    else    
+        glutSolidSphere(0.5, 30, 30);
 }
 
-void drawSphere(GLboolean vertical, GLfloat direction, GLboolean hand) {
+void drawSphere(GLboolean vertical, GLfloat direction) {
     glMatrixMode(GL_MODELVIEW);
 
-    if(arm_index == 0)
+    if(arm_index == 0) //from the previous exercise
     {
         glPushMatrix();
         glTranslatef(0.0f, 0.0f, 0.0f);
@@ -72,12 +82,12 @@ void drawSphere(GLboolean vertical, GLfloat direction, GLboolean hand) {
             glTranslatef(1.5f * direction, 0.0f, 0.0f);
     }
 
-    if(hand)
-        glColor3f(0.0f, 1.0f, 0.0f); // Color verde
-    else
-        glColor3f(1.0f, 0.0f, 0.0f); // Color rojo
+    glColor3f(1.0f, 0.0f, 0.0f); // Color rojo
 
-    glutSolidSphere(0.5, 30, 30);
+    if(wired)
+        glutWireSphere(0.5, 10, 10);
+    else    
+        glutSolidSphere(0.5, 30, 30);
 }
 
 void drawArmCube(GLboolean vertical, GLfloat direction)
@@ -106,63 +116,68 @@ void drawArmCube(GLboolean vertical, GLfloat direction)
 
     glColor3f(0.0f, 0.0f, 1.0f); // Color azul
 
-    // Cara frontal
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glEnd();
-
-    // Cara trasera
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
-
-    if(vertical)
-        glColor3f(0.0f, 1.0f, 0.0f); // Color verde
+    if(wired)
+        glutWireCube(1.0f);
     else
-        glColor3f(0.0f, 0.0f, 1.0f); // Color azul
+    {
+        // Cara frontal
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glEnd();
 
-    // Cara superior
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
+        // Cara trasera
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    // Cara fondo
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
+        if(vertical)
+            glColor3f(0.0f, 1.0f, 0.0f); // Color verde
+        else
+            glColor3f(0.0f, 0.0f, 1.0f); // Color azul
 
-    if(vertical)
-        glColor3f(0.0f, 0.0f, 1.0f); // Color azul
-    else
-        glColor3f(0.0f, 1.0f, 0.0f); // Color verde
+        // Cara superior
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    // Cara derecha
-    glBegin(GL_QUADS);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara fondo
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
 
-    // Cara izquierda
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
+        if(vertical)
+            glColor3f(0.0f, 0.0f, 1.0f); // Color azul
+        else
+            glColor3f(0.0f, 1.0f, 0.0f); // Color verde
+
+        // Cara derecha
+        glBegin(GL_QUADS);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glEnd();
+
+        // Cara izquierda
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
+    }    
 
     glPopMatrix();
 }
@@ -178,61 +193,66 @@ void drawHandCube(GLboolean vertical, GLfloat direction)
 
     glColor3f(0.0f, 1.0f, 1.0f); // Color cian
 
-    // Cara frontal
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glEnd();
+    if(wired)
+        glutWireCube(1.0f);
+    else    
+    {
+        // Cara frontal
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glEnd();
 
-    // Cara trasera
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
+        // Cara trasera
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    if(vertical)
-        glColor3f(1.0f, 0.0f, 0.0f); // Color rojo
+        if(vertical)
+            glColor3f(1.0f, 0.0f, 0.0f); // Color rojo
 
-    // Cara superior
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
+        // Cara superior
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    // Cara fondo
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara fondo
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
 
-    glColor3f(1.0f, 0.0f, 0.0f); // Color verde
+        glColor3f(1.0f, 0.0f, 0.0f); // Color verde
 
-    if(vertical)
-        glColor3f(0.0f, 1.0f, 1.0f); // Color cian
+        if(vertical)
+            glColor3f(0.0f, 1.0f, 1.0f); // Color cian
 
-    // Cara derecha
-    glBegin(GL_QUADS);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara derecha
+        glBegin(GL_QUADS);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glEnd();
 
-    // Cara izquierda
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara izquierda
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
+    }
 }
 
 void drawBodyCube()
@@ -247,77 +267,86 @@ void drawBodyCube()
     glRotatef(anglesZ[pieces], 0.0f, 0.0f, 1.0f);
 
     glPushMatrix();
-    glScalef(2.0f, 4.0f, 1.0f);
+    glScalef(3.0f, 5.0f, 1.0f);
 
     glColor3f(0.0f, 1.0f, 1.0f); // Color cian
 
-    // Cara frontal
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glEnd();
+    if(wired)
+        glutWireCube(1.0f);
+    else
+    {
+        // Cara frontal
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glEnd();
 
-    // Cara trasera
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
+        // Cara trasera
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    glColor3f(0.0f, 1.0f, 0.0f); // Color verde
+        glColor3f(0.0f, 1.0f, 0.0f); // Color verde
 
-    // Cara superior
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glEnd();
+        // Cara superior
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glEnd();
 
-    // Cara fondo
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara fondo
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
 
-    // Cara derecha
-    glBegin(GL_QUADS);
-    glVertex3f(0.5f, -0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, 0.5f);
-    glVertex3f(0.5f, 0.5f, -0.5f);
-    glVertex3f(0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara derecha
+        glBegin(GL_QUADS);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glEnd();
 
-    // Cara izquierda
-    glBegin(GL_QUADS);
-    glVertex3f(-0.5f, -0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, 0.5f);
-    glVertex3f(-0.5f, 0.5f, -0.5f);
-    glVertex3f(-0.5f, -0.5f, -0.5f);
-    glEnd();
+        // Cara izquierda
+        glBegin(GL_QUADS);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glEnd();
+    }
 
     pieces++;
     glPopMatrix();
 }
 
-void drawExtemity(GLboolean vertical, GLfloat direction, GLfloat x, GLfloat y, GLfloat z)
+void drawExtremity(GLboolean vertical, GLfloat direction, GLfloat x, GLfloat y, GLfloat z)
 {
-    drawShiftedSphere(x, y, z);
+    drawJoint(x, y, z); // move the sphere to the shoulder or hip
     drawArmCube(vertical, direction);
-    arm_index++;
-    drawSphere(vertical, direction, GL_FALSE);
+
+    arm_index++; // now the lower part of the extremity
+
+    drawSphere(vertical, direction);
     drawArmCube(vertical, direction);
-    //drawSphere(vertical, direction, GL_TRUE);
     drawHandCube(vertical, direction);
-    arm_index = 0;
+
+    arm_index = 0; // reset for the next extremity
+
+    pieces = pieces + 2; // add the two pieces to the register
 
     glPopMatrix();
-    pieces = pieces + 2;
+    
 }
 
 void display()
@@ -325,18 +354,22 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    drawBodyCube();
+    drawBodyCube(); //piece correconding to p0
 
-    //pieces = 0;
+    // Draw the legs
+    drawExtremity(GL_TRUE, 1.0, -1.0, -2.5, 0.0); //pieces correconding to p1 & p2
+    drawExtremity(GL_TRUE, 1.0, 1.0, -2.5, 0.0); //pieces correconding to p3 & p4
 
-    // Legs
-    drawExtemity(GL_TRUE, 1.0, -0.5, -2.5, 0.0);
-    drawExtemity(GL_TRUE, 1.0, 0.5, -2.5, 0.0);
+    // Draw the arms
+    drawExtremity(GL_FALSE, -1.0, -1.5, 2.0, 0.0); //pieces correconding to p5 & p6
+    drawExtremity(GL_FALSE, 1.0, 1.5, 2.0, 0.0); //pieces correconding to p7 & p8
 
-    // Arms
-    drawExtemity(GL_FALSE, -1.0, -1.5, 1.5, 0.0);
-    drawExtemity(GL_FALSE, 1.0, 1.5, 1.5, 0.0);
-
+    if(spin) // if spin is active spin the lower part of the legs
+    {
+        anglesZ[2] += spinSpeed;
+        anglesZ[4] += spinSpeed;
+    }
+    
     glFlush();
     glutSwapBuffers();
 }
@@ -354,7 +387,8 @@ void idle()
     display();
 }
 
-void keyboard(unsigned char key, int x, int y) {
+void keyboard(unsigned char key, int x, int y) 
+{
     switch (key) 
     {
         case '1':
@@ -381,10 +415,97 @@ void keyboard(unsigned char key, int x, int y) {
             anglesZ[piece_index] -= 1.0f;
             break;
     }
-
     glutPostRedisplay();
 }
- 
+
+void main_menu(int id){
+    switch( id){
+    case 0:
+        if(spin)
+            spin = GL_FALSE;
+        else
+            spin = GL_TRUE;    
+        break;
+    case 1:
+        exit(-1);
+        break;
+    }
+}
+
+void view_menu(int id){
+    switch( id){
+    case 0:
+        wired = GL_TRUE;
+        break;
+    case 1:
+        wired = GL_FALSE;
+        break;
+    }
+}
+
+void speed_menu(int id){
+    switch( id){
+    case 0:
+        spinSpeed = 1.0f;
+        break;
+    case 1:
+        spinSpeed = 2.0f;
+        break;
+    case 2:
+        spinSpeed = 5.0f;
+        break;    
+    case 3:
+        spinSpeed = 10.0f;
+        break;     
+    }
+}
+
+void CallMenu()
+{
+    int submenu = glutCreateMenu(view_menu);
+    glutAddMenuEntry("Alamabre", 0);
+    glutAddMenuEntry("Solidos", 1);
+
+    int submenu2 = glutCreateMenu(speed_menu);
+    glutAddMenuEntry("Despacio", 0);
+    glutAddMenuEntry("Normal", 1);
+    glutAddMenuEntry("Rapido", 2);
+    glutAddMenuEntry("Turbo", 3);
+
+    glutCreateMenu(main_menu);
+    glutAddSubMenu("Visualizacion", submenu);
+    glutAddMenuEntry("Giro automatico", 0);
+    glutAddSubMenu("Velocidad de giro", submenu2);
+    glutAddMenuEntry("Salir", 1);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+void ClickNDrag(int button, int state, int x, int y) {
+    int selectedPiece = 0;
+
+    if(glutGetModifiers() == GLUT_ACTIVE_CTRL)
+        selectedPiece = 1;
+    else if(glutGetModifiers() == GLUT_ACTIVE_ALT)
+        selectedPiece = 3;
+
+    int frameX;
+    int frameY;
+
+    if(state == GLUT_DOWN)  
+    {
+        frameX = x;
+        frameY = y; 
+    }  
+
+    if(state == GLUT_UP)  
+    {
+        anglesZ[selectedPiece] += x - frameX;
+        anglesX[selectedPiece] += y - frameY;
+    } 
+      
+}
+
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -398,7 +519,12 @@ int main(int argc, char **argv)
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutIdleFunc(idle);
+
+    glutMouseFunc(ClickNDrag);
+
     glutKeyboardFunc(keyboard);
+
+    CallMenu();
     glutMainLoop();
 
     return 0;
