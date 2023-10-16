265e89f6a10807a5e8b8329e27aa9a42d6e82bd5
Meleagrista <martin.dorio@udc.es>
2023-10-16 23:18:19 +0200

    More testing...
    Finished one of the exams practices.

--- exams/e1.c
+++ exams/e1.c
@@ -9,9 +9,18 @@
 GLint ancho = 400;
 GLint alto = 400;
 
-float angleX = 0.0f;
-float angleY = 0.0f;
-float angleZ = 0.0f;
+int frameX;
+int frameY;
+
+GLfloat coneAngle[] = {0.0f, 0.0f};
+GLfloat objectAngle = 0.0f;
+GLfloat armAngle = 0.0f;
+
+GLboolean coneSpin = GL_TRUE;
+GLboolean coneSpinReverse = GL_FALSE;
+GLfloat coneSpinSpeed = 1.0f;
+
+GLfloat cubeColors[] = {0.949f, 0.937f, 0.694f, 0.886f, 0.874f, 0.549f};
 
 // Variable to control whether to use perspective projection
 int hazPerspectiva = 0;
@@ -44,7 +53,7 @@ void drawFourCubes(GLdouble base, GLdouble space)
                 break;            
         }
         
-        glColor3f(0.949f, 0.937f, 0.694f); // Light Pastel Yellow
+        glColor3f(cubeColors[0], cubeColors[1], cubeColors[2]);
 
         // Cara frontal
         glBegin(GL_QUADS);
@@ -62,7 +71,7 @@ void drawFourCubes(GLdouble base, GLdouble space)
         glVertex3f(-size, size, -size);
         glEnd();
 
-        glColor3f(0.886f, 0.874f, 0.549f); // Darker Pastel Yellow
+        glColor3f(cubeColors[3], cubeColors[4], cubeColors[5]);
 
         // Cara frontal
         glBegin(GL_QUADS);
@@ -80,7 +89,7 @@ void drawFourCubes(GLdouble base, GLdouble space)
         glVertex3f(-size, -size, -size);
         glEnd();
 
-        glColor3f(0.886f, 0.874f, 0.549f); // Darker Pastel Yellow
+        glColor3f(cubeColors[3], cubeColors[4], cubeColors[5]);
 
         // Cara derecha
         glBegin(GL_QUADS);
@@ -103,20 +112,12 @@ void drawFourCubes(GLdouble base, GLdouble space)
 
 }
 
-void drawInverseCone(GLdouble radious, GLdouble height, GLboolean axis, GLfloat angle_correction)
+void drawInverseCone(GLdouble radious, GLdouble height, GLboolean axis)
 {
-    glRotatef(90.0 - angle_correction, 1.0f, 0.0f, 0.0f);
+    if(axis)
+        glRotatef(coneAngle[1], 0.0f, 0.0f, 1.0f);
 
     glPushMatrix();
-
-    //glTranslatef(0.0f, 0.0f, - height / 2);
-
-    if(axis)
-    {
-        glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-        glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-        glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
-    }
     
     glColor3f(0.678f, 0.847f, 0.706f); // Light Pastel Green
 
@@ -130,18 +131,15 @@ void drawInverseCone(GLdouble radious, GLdouble height, GLboolean axis, GLfloat 
     glutSolidCone(radious, height, 20, 20); // Draw a solid cone
 
     glPopMatrix();
-
-    if(axis)
-    {
-        glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-        glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-        glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
-    }
 }
 
-void drawArm(GLdouble base, GLdouble length)
+void drawArm(GLdouble base, GLdouble length, GLboolean axis)
 {
+    if(axis)
+        glRotatef(armAngle, 0.0f, 0.0f, 1.0f);
+
     glPushMatrix();
+    
     glTranslatef(0.0f, (length * base)/2, 0.0f);
     glScalef(1.0f, length, 1.0f);
 
@@ -206,24 +204,10 @@ void drawArm(GLdouble base, GLdouble length)
     glPopMatrix();
 }
 
-void drawTestUnit(GLdouble height)
+void drawCone(GLdouble radious, GLdouble height, GLboolean axis)
 {
-    glLoadIdentity();
-    glTranslatef(0.0f, height + 1.0f, 0.0f);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glutSolidSphere(1.0f, 20, 20);
-}
-
-void drawCone(GLdouble radious, GLdouble height, GLboolean axis, GLfloat angle_correction)
-{
-    glRotatef(270.0 - angle_correction, 1.0f, 0.0f, 0.0f);
-
     if(axis)
-    {
-        glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-        glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-        glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
-    }
+        glRotatef(coneAngle[0], 0.0f, 0.0f, 1.0f);
     
     // Light Pastel Blue
     glColor3f(0.7f, 0.9f, 1.0f);
@@ -239,18 +223,14 @@ void drawCone(GLdouble radious, GLdouble height, GLboolean axis, GLfloat angle_c
     glutSolidCone(radious, height, 20, 20); // Draw a solid cone
 }
 
-void drawCylinder(GLdouble radious, GLdouble height, GLboolean axis, GLfloat angle_correction)
+void drawCylinder(GLdouble radious, GLdouble height, GLboolean axis)
 {
-    glRotatef(90.0 - angle_correction, 1.0f, 0.0f, 0.0f);
-
     // Adjustment for the center of mass
     glTranslatef(0.0f, 0.0f, height / 2);
+
     if(axis)
-    {
-        glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-        glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-        glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
-    }
+        glRotatef(objectAngle, 1.0f, 0.0f, 0.0f);
+
     glTranslatef(0.0f, 0.0f, - height / 2);
 
     // Light Pastel Orange
@@ -272,42 +252,151 @@ void drawCylinder(GLdouble radious, GLdouble height, GLboolean axis, GLfloat ang
     glutSolidCylinder(radious, height, 20, 20);
 }
 
-void axisRotation()
-{
-    glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-    glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-    glRotatef(angleZ, 0.0f, 0.0f, 1.0f);
-}
+
+// INTERFACE AND UTILITY FUNCTIONS
 
 void keyboard(unsigned char key, int x, int y) 
 {
     switch (key) 
     {
-        case 'q':
-            angleX += 1.0f;
-            break;
-        case 'a':
-            angleX -= 1.0f;
-            break;
-        case 'w':
-            angleY += 1.0f;
-            break;
         case 's':
-            angleY -= 1.0f;
+            coneSpin = !coneSpin;
             break;
-        case 'e':
-            angleZ += 1.0f;
+        case 'r':
+            coneSpinReverse = !coneSpinReverse;
+            break; 
+        case 'p':
+            hazPerspectiva = 1;
             break;
-        case 'd':
-            angleZ -= 1.0f;
-            break;
+        case 'o':
+            hazPerspectiva = 0;
+            break;            
     }
     glutPostRedisplay();
 }
 
+void spin()
+{
+    if(coneSpin)
+    {
+        if(coneSpinReverse)
+            coneAngle[0] += coneSpinSpeed;
+        else
+            coneAngle[0] -= coneSpinSpeed;
+        coneAngle[1] += coneSpinSpeed * 2;
+    }
+}
+
+void rotate(int button, int state, int x, int y) {
+    int frameX;
+    int frameY;
+    
+    if(glutGetModifiers() == GLUT_ACTIVE_ALT)
+    {
+        if(state == GLUT_DOWN)  
+            frameY = y; 
+        if(state == GLUT_UP)  
+            objectAngle += y - frameY;
+    }
+}
+
+void basculate(int x, int y) {
+    int angleLimit = 30;
+    int angleChange = x - frameX;
+
+    if ((angleChange > 0 && armAngle < angleLimit) || (angleChange < 0 && armAngle > -angleLimit))
+        armAngle += (x - frameX) / 2;
+
+    frameX = x;
+    frameY = y;
+}
+
+void speed_menu(int id){
+    switch( id){
+    case 0:
+        coneSpinSpeed = 1.0f;
+        break;
+    case 1:
+        coneSpinSpeed = 4.0f;
+        break;   
+    }
+}
+
+void color_menu(int id) {
+    switch (id) {
+    case 0:
+        // Light pastel yellow
+        cubeColors[0] = 0.949f;
+        cubeColors[1] = 0.937f;
+        cubeColors[2] = 0.694f;
+        // Darker yellow
+        cubeColors[3] = 0.886f;
+        cubeColors[4] = 0.874f;
+        cubeColors[5] = 0.549f;
+        break;
+    case 1:
+        // Light pastel red
+        cubeColors[0] = 0.949f;
+        cubeColors[1] = 0.788f;
+        cubeColors[2] = 0.788f;
+        // Darker red
+        cubeColors[3] = 0.749f;
+        cubeColors[4] = 0.396f;
+        cubeColors[5] = 0.396f;
+        break;
+    case 2:
+        // Light pastel blue
+        cubeColors[0] = 0.690f;
+        cubeColors[1] = 0.878f;
+        cubeColors[2] = 0.902f;
+        // Darker blue
+        cubeColors[3] = 0.529f;
+        cubeColors[4] = 0.725f;
+        cubeColors[5] = 0.752f;
+        break;
+    case 3:
+        // Light pastel green
+        cubeColors[0] = 0.678f;
+        cubeColors[1] = 0.847f;
+        cubeColors[2] = 0.706f;
+        // Darker green
+        cubeColors[3] = 0.549f;
+        cubeColors[4] = 0.729f;
+        cubeColors[5] = 0.529f;
+        break;
+    }
+}
+
+void main_menu(int id){
+    switch( id){
+    case 0:
+        exit(-1);
+        break;
+    }
+}
+
+void menu()
+{
+    int submenu = glutCreateMenu(color_menu);
+    glutAddMenuEntry("Amarillo", 0);
+    glutAddMenuEntry("Rojo", 1);
+    glutAddMenuEntry("Azul", 2);
+    glutAddMenuEntry("Verde", 3);
+
+    int submenu2 = glutCreateMenu(speed_menu);
+    glutAddMenuEntry("Normal", 0);
+    glutAddMenuEntry("Rapido", 1);
+
+    glutCreateMenu(main_menu);
+    glutAddSubMenu("Colores", submenu);
+    glutAddSubMenu("Velocidad", submenu2);
+    glutAddMenuEntry("Salir", 0);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+
 // PREDEFINED FUNCTIONS
 
-// Function to handle window resizing
 void reshape(int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -323,40 +412,43 @@ void reshape(int width, int height)
 
     glMatrixMode(GL_MODELVIEW);
  
-    // Update the global window dimensions
     ancho = width;
     alto = height;
 }
 
-// Function to render the scene
 void display()
 { 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
+    spin();
+
     float cone_height = 3.0f;
 
-    drawCylinder(4.0, 1.5, GL_TRUE, 0.0);
-    drawCone(3.5, cone_height, GL_FALSE, 90.0);
+    glRotatef(90.0, 1.0f, 0.0f, 0.0f); // angle correction
+    drawCylinder(4.0, 1.5, GL_TRUE);
+
+    glRotatef(180.0f, 1.0f, 0.0f, 0.0f); // angle correction
+    drawCone(3.5, cone_height, GL_TRUE);
 
     float cube_size = 1.0f;
     float cube_length = 6.0f;
 
-    // Position adjustments
     glRotatef(90.0, 1.0f, 0.0f, 0.0f);
-    glTranslatef(0.0, cone_height + cube_size / 2, 0.0f);
+    glTranslatef(0.0, cone_height, 0.0f);
     glRotatef(-90.0, 0.0f, 0.0f, 1.0f);
     
-    drawArm(cube_size, cube_length);
+    drawArm(cube_size, cube_length, GL_TRUE);
 
     float small_cone_height = 2.0f;
 
-    // Position adjustments
     glTranslatef(0.0, cube_length * cube_size, 0.0f);
-    glTranslatef(- small_cone_height + cube_size / 2, 0.0f, 0.0f);
+    glRotatef(-armAngle, 0.0f, 0.0f, 1.0f); // balance correction
+    glTranslatef(- small_cone_height, 0.0f, 0.0f);
     glRotatef(90.0, 0.0f, 0.0f, 1.0f);
+    glRotatef(90.0, 1.0f, 0.0f, 0.0f); // angle correction
 
-    drawInverseCone(2.5f, small_cone_height, GL_FALSE, 0.0);
+    drawInverseCone(2.5f, small_cone_height, GL_TRUE);
 
     drawFourCubes(1.0f, 1.0f);
 
@@ -364,7 +456,6 @@ void display()
     glutSwapBuffers();
 }
 
-// Function to initialize OpenGL settings
 void init()
 {
     glClearColor(0, 0, 0, 0);
@@ -373,7 +464,6 @@ void init()
     alto = 400;
 }
 
-// Function to continuously update the display
 void idle()
 {
     display();
@@ -390,14 +480,15 @@ int main(int argc, char **argv)
 
     init();
 
-    // Register callback functions
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutIdleFunc(idle);
 
+    glutMouseFunc(rotate);
+    glutPassiveMotionFunc(basculate); //TODO: Arreglar el error.
     glutKeyboardFunc(keyboard);
+    menu(); //TODO: Arreglar el error.
 
-    // Start the main loop
     glutMainLoop();
 
     return 0;
